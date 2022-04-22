49eb966650f49298addc9e754e2928409d3acca2
Kapitolina <kapitolina999@gmail.com>
2022-04-22 11:06:10 +0500

    fix: add <pre>

--- main.py
+++ main.py
@@ -8,20 +8,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def page_index():
-    return f'<pre>{candidates.get_data_candidates()}'
+    return f'<pre>{candidates.get_data_candidates()}<pre>'
 
 
 @app.route('/candidates/<int:uid>')
 def page_profile(uid):
-    return f'<pre>{candidates.get_data_candidate(uid)}'
+    return f'<pre>{candidates.get_data_candidate(uid)}<pre>'
 
 
 @app.route('/skills/<x>')
 def page_skills(x):
-    return f'<pre>{candidates.get_candidates_with_skill(x)}'
+    return f'<pre>{candidates.get_candidates_with_skill(x)}<pre>'
 
 
 app.run()
-
-
 
